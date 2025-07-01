bc0368be0c587b76ce0826094b7b538b2dc66183
Abhishek Thakur <abburajput7876@gmail.com>
2025-07-01 16:56:13 +0530

    my first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+  <h1 className="text-red-300">hey my name is abhishek</h1>
+    </>
+  )
+}
+
+export default App
