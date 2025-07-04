b2280982f1d981228a4b2044b7d4f9d301f32b62
Abhishek Thakur <abburajput7876@gmail.com>
2025-07-04 10:31:09 +0530

    setUp signin & signup Api..

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,24 @@
 import React, { use, useRef, useState } from "react";
 import Header from "./Header";
 import { checkValidatedata } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/Firebase";
 
 const Login = () => {
+  const [isSignInForm, setisSignInForm] = useState(true);
+  const [ShowError, SetShowError] = useState(null);
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef(null);
 
-const [isSignInForm, setisSignInForm] = useState(true);
-const [ShowError, SetShowError] = useState(null);
-const email = useRef(null);
-const password = useRef(null);
-const name = useRef(null);
+  const toggleSignin = () => {
+    setisSignInForm(!isSignInForm);
+  };
 
-const toggleSignin= ()=>{
-  setisSignInForm(!isSignInForm);
-};
-                                
-
-const handleButtonClick = ()=>{
+  const handleButtonClick = () => {
     // Validate the form data
     const Email = email.current.value;
     const Password = password.current.value;
@@ -24,76 +27,103 @@ const handleButtonClick = ()=>{
     console.log(Email);
     console.log(Password);
     console.log(Name);
-    
-    const msg = checkValidatedata(Email,Password,Name,isSignInForm); 
+
+    const msg = checkValidatedata(Email, Password, Name, isSignInForm);
     SetShowError(msg);
 
-    // signin /Signup
-     
-}
+    if (msg) return;
 
+    if (!isSignInForm) {
+      // Signed up
+      createUserWithEmailAndPassword(auth, Email, Password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          SetShowError(errorCode + "_" + errorMessage);
+        });
+    } else {
+      signInWithEmailAndPassword(auth, Email, Password)
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          // ...
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          if("auth/invalid-credential_Firebase: Error (auth/invalid-credential)." == errorCode + "_" + errorMessage){
+            SetShowError("User not found. Please try again..");
+          }
+          
+        });
+    }
+  };
 
-return (
-  <div className="relative min-h-screen">
-    <Header />
+  return (
+    <div className="relative min-h-screen">
+      <Header />
 
-    {/* Background Image */}
-    <img
-      className="absolute inset-0 w-full h-full object-cover -z-10"
-      src="https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_small.jpg"
-      alt="background Image"
-    />
+      {/* Background Image */}
+      <img
+        className="absolute inset-0 w-full h-full object-cover -z-10"
+        src="https://assets.nflxext.com/ffe/siteui/vlv3/75b0ed49-75ab-4a63-bd45-37bc2c95cb73/web/IN-en-20250623-TRIFECTA-perspective_ae5833b7-6ce5-4e88-853e-014f38c506f1_small.jpg"
+        alt="background Image"
+      />
 
-    {/* Form */}
-    <form
-      onSubmit={(e) => e.preventDefault()}
-      className="w-11/12 sm:w-9/12 md:w-6/12 lg:w-4/12 xl:w-3/12 p-6 sm:p-8 bg-black/80 rounded-md text-white absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
-    >
-      <h1 className="font-semibold text-2xl sm:text-3xl pb-6">
-        {isSignInForm ? "Sign In" : "Sign Up"}
-      </h1>
+      {/* Form */}
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-11/12 sm:w-9/12 md:w-6/12 lg:w-4/12 xl:w-3/12 p-6 sm:p-8 bg-black/80 rounded-md text-white absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2"
+      >
+        <h1 className="font-semibold text-2xl sm:text-3xl pb-6">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
 
-      {!isSignInForm && (
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-3 mb-4 w-full rounded-sm bg-gray-700 placeholder-gray-400"
+          />
+        )}
+
         <input
-          ref={name}
+          ref={email}
           type="text"
-          placeholder="Full Name"
+          placeholder="Email"
           className="p-3 mb-4 w-full rounded-sm bg-gray-700 placeholder-gray-400"
         />
-      )}
 
-      <input
-        ref={email}
-        type="text"
-        placeholder="Email"
-        className="p-3 mb-4 w-full rounded-sm bg-gray-700 placeholder-gray-400"
-      />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-3 mb-6 w-full rounded-sm bg-gray-700 placeholder-gray-400"
+        />
 
-      <input
-        ref={password}
-        type="password"
-        placeholder="Password"
-        className="p-3 mb-6 w-full rounded-sm bg-gray-700 placeholder-gray-400"
-      />
+        <p className="text-red-500 p-2 font-bold">{ShowError}</p>
 
-      <p className="text-red-500 p-2 font-bold">{ShowError}</p>
+        <button
+          className="p-3 mb-4 bg-red-600 w-full rounded-sm hover:bg-red-700 font-semibold cursor-pointer"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
 
-      <button
-        className="p-3 mb-4 bg-red-600 w-full rounded-sm hover:bg-red-700 font-semibold cursor-pointer"
-        onClick={handleButtonClick}
-      >
-        {isSignInForm ? "Sign In" : "Sign Up"}
-      </button>
-
-      <p className="p-4 cursor-pointer" onClick={toggleSignin}>
-        {isSignInForm
-          ? "New to Netflix? Sign Up Now"
-          : "Already have an account, Sign In now"}
-      </p>
-    </form>
-  </div>
-);
-
+        <p className="p-4 cursor-pointer" onClick={toggleSignin}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already have an account, Sign In now"}
+        </p>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -1,16 +1,23 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCqMj8Hl4CTwNsajOp_8BJHf0aEvu9xWJs",
-  authDomain: "moviegpt-cd03c.firebaseapp.com",
-  projectId: "moviegpt-cd03c",
-  storageBucket: "moviegpt-cd03c.firebasestorage.app",
-  messagingSenderId: "128615034926",
-  appId: "1:128615034926:web:6d89ac544d8e9c1d8aefbf",
-  measurementId: "G-8L5CPS0J5J"
+  apiKey: "AIzaSyAclFH--M3WDS62tsH0Tmv-HL2uSZ0PweE",
+  authDomain: "moviegpt-39e2b.firebaseapp.com",
+  projectId: "moviegpt-39e2b",
+  storageBucket: "moviegpt-39e2b.firebasestorage.app",
+  messagingSenderId: "188790631483",
+  appId: "1:188790631483:web:7120b57c6a548ff081401e",
+  measurementId: "G-D8TG7EQBKQ"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+export const auth = getAuth();
