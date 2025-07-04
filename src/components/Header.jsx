c7c5bce4eb44dd4b3e1c32eeff07ce11abe0b975
Abhishek Thakur <abburajput7876@gmail.com>
2025-07-04 13:49:01 +0530

    small changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { auth } from "../utils/Firebase";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,6 +6,7 @@ import { removeUser } from "../utils/UserSlice";
 import { signOut } from "firebase/auth";
 
 const Header = () => {
+  const [isOpen, setIsOpen] = useState(false)
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const user = useSelector((store) => store.user);
@@ -25,8 +26,14 @@ const Header = () => {
       });
   };
 
+
+
+  const toggleDropdown = ()=>{
+    setIsOpen(!isOpen);
+  }
+
   return (
-    <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between">
+    <div className="absolute w-full px-8 py-2 bg-gradient-to-b from-black z-10 flex justify-between md:px-4">
       <img
         className="w-44"
         src="
@@ -35,16 +42,17 @@ https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c
       />
 
 
-
       {user && (
-        <div className="relative p-3 inline-block group">
+        <div className="relative py-4 inline-block group">
           <img
             className="w-10 h-10"
             src="https://occ-0-2590-2186.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABUi6ZpOspE7SWzYlN-o_H3QkoVd5zpDcPgQEQWWl73eQ9kxZiA9t6hgFjyw7ITQuAKIgfZdBkv02PdLFHos7NFC5SGlz-Nk.png?r=66c"
             alt="usericon"
+            onClick={toggleDropdown}
           ></img>
-          <div className="absolute hidden group-hover:block bg-black text-white shadow-lg mt-1 z-10 -right-1/4">
-            <ul className="w-48">
+          <div className={`absolute group-hover:block bg-black text-white ${isOpen ? 'block' : 'hidden'} shadow-lg mt-1 z-10 -right-1/4 py-10`}>
+            <div className="top-1">
+              <ul className="w-48">
               <li className="px-4 py-2 hover:bg-gray-800 cursor-pointer">
                  {user.displayName}, Account
               </li>
@@ -58,6 +66,7 @@ https://help.nflxext.com/helpcenter/OneTrust/oneTrust_production/consent/87b6a5c
                 signout from Netflix?
               </li>
             </ul>
+            </div>
           </div>
         </div>
       )}
